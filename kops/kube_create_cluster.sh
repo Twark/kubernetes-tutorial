26d8e7642b6a697cbb8060c7e39eff2a985fa4fa
Twark <nthierry@yahoo.fr>
2017-03-06 22:43:26 +0100

    Update kube_create_cluster.sh

--- kops/kube_create_cluster.sh
+++ kops/kube_create_cluster.sh
@@ -2,3 +2,4 @@ export KOPS_STATE_STORE=s3://kub-wavetesting-com-state-store
 kops create -f https://raw.githubusercontent.com/Twark/kubernetes-tutorial/master/kops/cluster_config
 kops create -f https://raw.githubusercontent.com/Twark/kubernetes-tutorial/master/kops/master_config
 kops create -f https://raw.githubusercontent.com/Twark/kubernetes-tutorial/master/kops/nodes_config
+kops create secret --name mycluster.wavetesting.com sshpublickey admin -i ~/.ssh/id_rsa.pub
